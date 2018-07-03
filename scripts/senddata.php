d46b98cd92a169a70e3e5712d373b0df5088c429
Gal Ben-Chanoch <gbenchanoch@gmail.com>
2018-07-03 11:39:24 -0400

    Testing PHP

--- scripts/senddata.php
+++ scripts/senddata.php
@@ -1,6 +1,6 @@
 <?php
 	$data = JSON.stringify(vm.model);
-    $ret = file_put_contents('tmp/mydata.txt', $data, FILE_APPEND | LOCK_EX);
+    $ret = file_put_contents('/mydata.txt', $data, FILE_APPEND | LOCK_EX);
     if($ret === false) {
         die('There was an error writing this file');
     }
